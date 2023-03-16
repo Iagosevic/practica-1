c59ca53b07ec147c9f4c84f7d5d6bb3ee5ae7f85
Iagosevic <micromachine13@hotmail.com>
2023-03-16 14:09:15 +0100

    fix: añadidas las propierties

--- src/main/java/com/qindel/practicas/practica1/Practica1Application.java
+++ src/main/java/com/qindel/practicas/practica1/Practica1Application.java
@@ -3,13 +3,15 @@ package com.qindel.practicas.practica1;
 import com.qindel.practicas.practica1.repositories.JJOORepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 @EnableJpaRepositories("com.qindel.practicas.practica1.repositories")
+@EntityScan("com.qindel.practicas.practica1.entities")
 //@EnableJpaRepositories(basePackageClasses = JJOORepository.class)
 public class Practica1Application {
 	public static void main(String[] args) {
