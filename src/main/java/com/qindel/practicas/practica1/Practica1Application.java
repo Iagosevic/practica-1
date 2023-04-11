b88e925af6a557a3ed70c4a98444968edf3d1b9e
Iagosevic <micromachine13@hotmail.com>
2023-04-11 09:53:35 +0200

    fix: cambiadas las notaciones en el main

--- src/main/java/com/qindel/practicas/practica1/Practica1Application.java
+++ src/main/java/com/qindel/practicas/practica1/Practica1Application.java
@@ -3,10 +3,12 @@ package com.qindel.practicas.practica1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.qindel.practicas.practica1.entities"})
+@ComponentScan(basePackages = "com.qindel.practicas.practica1.*")
+@EntityScan("com.qindel.practicas.practica1.*")
 @EnableJpaRepositories(basePackages = "com.qindel.practicas.practica1.repositories")
 public class Practica1Application {
 	public static void main(String[] args) {
