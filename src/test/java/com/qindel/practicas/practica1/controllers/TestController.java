d7aa8f590f963b8ba1b896146614fe4761a17238
Iagosevic <micromachine13@hotmail.com>
2023-04-17 13:02:50 +0200

    fix:eliminados imports y arreglado fallo

--- src/test/java/com/qindel/practicas/practica1/controllers/TestController.java
+++ src/test/java/com/qindel/practicas/practica1/controllers/TestController.java
@@ -1,22 +1,5 @@
 package com.qindel.practicas.practica1.controllers;
 
-import com.qindel.practicas.practica1.apirest.CiudadDto;
-import com.qindel.practicas.practica1.entities.CiudadEntity;
-import com.qindel.practicas.practica1.mapper.ICiudadMapper;
-import com.qindel.practicas.practica1.repositories.ICiudadRepository;
-import com.qindel.practicas.practica1.services.impl.CiudadServiceImpl;
-import org.apache.logging.log4j.util.Strings;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class TestController {
 
 }
