5329154679b8ac5aaed642279298af8285a32591
Iagosevic <micromachine13@hotmail.com>
2023-04-25 12:38:30 +0200

    fix: arreglados nombres de variables incorrectos

--- src/test/java/com/qindel/practicas/practica1/services/CiudadServiceImplTest.java
+++ src/test/java/com/qindel/practicas/practica1/services/CiudadServiceImplTest.java
@@ -16,7 +16,7 @@ import static org.mockito.Mockito.times;
 
 public class CiudadServiceImplTest {
     @Mock
-    private ICiudadRepository ciudadRepository;
+    private ICiudadRepository mockCiudadRepository;
 
     @InjectMocks
     private ICiudadService ciudadService;
@@ -32,15 +32,16 @@ public class CiudadServiceImplTest {
         Integer id_pais = Integer.parseInt(id_paisString);
         Integer valor_ciudad = Integer.parseInt(valor_ciudadString);
 
-        CiudadEntity ciudad = new CiudadEntity(id_ciudad,nombre_ciudad,id_ciudad,valor_ciudad);
-        Mockito.when(ciudadRepository.getReferenceById(id_ciudad)).thenReturn(ciudad);
+        CiudadEntity ciudad = new CiudadEntity(id_ciudad,nombre_ciudad,id_pais,valor_ciudad);
 
-        CiudadDto ciudadDto =  new CiudadDto(id_ciudad,nombre_ciudad,id_ciudad,valor_ciudad);
+        Mockito.when(mockCiudadRepository.getReferenceById(id_ciudad)).thenReturn(ciudad);
+
+        CiudadDto ciudadDto =  new CiudadDto(id_ciudad,nombre_ciudad,id_pais,valor_ciudad);
         Mockito.when(ciudadMapper.toDto(ciudad)).thenReturn(ciudadDto);
 
         CiudadDto ciudadDtoExpected = ciudadService.getCiudadByIdCiudad(id_ciudad);
 
-        Mockito.verify(ciudadRepository, times(1)).getReferenceById(id_ciudad);
+        Mockito.verify(mockCiudadRepository, times(1)).getReferenceById(id_ciudad);
         assertThat(ciudadDto).usingRecursiveComparison().isEqualTo(ciudadDtoExpected);
 
     }
@@ -49,18 +50,18 @@ public class CiudadServiceImplTest {
     @CsvSource({"1,A Corunha,1,93", "8,Paris,3,5"})
     public void addCiudadTest(String id_ciudadString, String nombre_ciudad, String id_paisString, String valor_ciudadString) {
         Integer id_ciudad = Integer.parseInt(id_ciudadString);
-        Integer id_pais = Integer.parseInt(id_ciudadString);
+        Integer id_pais = Integer.parseInt(id_paisString);
         Integer valor_ciudad = Integer.parseInt(valor_ciudadString);
 
-        CiudadEntity ciudad = new CiudadEntity(id_ciudad, nombre_ciudad, id_ciudad, valor_ciudad);
-        Mockito.when(ciudadRepository.save(ciudad)).thenReturn(ciudad);
+        CiudadEntity ciudad = new CiudadEntity(id_ciudad, nombre_ciudad, id_pais, valor_ciudad);
+        Mockito.when(mockCiudadRepository.save(ciudad)).thenReturn(ciudad);
 
-        CiudadDto ciudadDto = new CiudadDto(id_ciudad, nombre_ciudad, id_ciudad, valor_ciudad);
+        CiudadDto ciudadDto = new CiudadDto(id_ciudad, nombre_ciudad, id_pais, valor_ciudad);
         Mockito.when(ciudadMapper.toDto(ciudad)).thenReturn(ciudadDto);
         Mockito.when(ciudadMapper.toEntity(ciudadDto)).thenReturn(ciudad);
 
         CiudadDto ciudadDtoExpected = ciudadService.addCiudad(ciudadDto);
-        Mockito.verify(ciudadRepository, times(1)).save(ciudad);
+        Mockito.verify(mockCiudadRepository, times(1)).save(ciudad);
         assertThat(ciudadDto).usingRecursiveComparison().isEqualTo(ciudadDtoExpected);
     }
 
@@ -68,16 +69,16 @@ public class CiudadServiceImplTest {
     @CsvSource({"14,Elche,1,309"})
     public CiudadDto updateCiudad(CiudadDto ciudadDto, Integer idCiudad) {
         CiudadEntity newCiudad = ciudadMapper.toEntity(ciudadDto);
-        return ciudadMapper.toDto(ciudadRepository.findById(idCiudad)
+        return ciudadMapper.toDto(mockCiudadRepository.findById(idCiudad)
                 .map(ciudad -> {
                     ciudad.setIdciudad(newCiudad.getIdciudad());
                     ciudad.setIdpais(newCiudad.getIdpais());
                     ciudad.setNombreciudad(newCiudad.getNombreciudad());
                     ciudad.setValorciudad(newCiudad.getValorciudad());
-                    return ciudadRepository.save(ciudad);
+                    return mockCiudadRepository.save(ciudad);
                 }).orElseGet(() -> {
                     newCiudad.setIdciudad(idCiudad);
-                    return ciudadRepository.save(newCiudad);
+                    return mockCiudadRepository.save(newCiudad);
                 })
         );
     }
