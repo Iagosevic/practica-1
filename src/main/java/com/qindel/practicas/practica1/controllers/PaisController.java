f426517f005cc6d39a85b5675944885ed024aab8
Iagosevic <micromachine13@hotmail.com>
2023-06-01 11:38:09 +0200

    feat: añadidas anotaciones de roles

--- src/main/java/com/qindel/practicas/practica1/controllers/PaisController.java
+++ src/main/java/com/qindel/practicas/practica1/controllers/PaisController.java
@@ -5,6 +5,7 @@ import com.qindel.practicas.practica1.apirest.PaisDto;
 import com.qindel.practicas.practica1.entities.PaisEntity;
 import com.qindel.practicas.practica1.services.IPaisService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,19 +17,22 @@ public class PaisController {
 
     @Autowired
     private IPaisService paisService;
-
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("")
     public List<PaisDto> getPaises(){
         return paisService.getAllPaises();
     }
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/{id-pais}")
     public PaisDto getpaisById(@PathVariable("id-pais") Integer idpais){
         return paisService.getPaisByIdPais(idpais);
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/pais")
     public PaisDto addPais(@RequestBody PaisDto nuevoPais) {
         return paisService.addPais(nuevoPais);
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/pais/{id-pais}")
     public PaisDto updatePais(@PathVariable("id-pais") Integer idpais, @RequestBody PaisDto nuevoPais) {
         PaisDto paisEditar = paisService.getPaisByIdPais(idpais);
@@ -39,7 +43,7 @@ public class PaisController {
 
         return paisService.updatePais(nuevoPais, idpais);
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/pais/{id-pais}")
     public void deletePais(@PathVariable("id-pais") Integer idpais) {
         paisService.deletePais(idpais);
--- src/main/java/com/qindel/practicas/practica1/controllers/TipoController.java
+++ src/main/java/com/qindel/practicas/practica1/controllers/TipoController.java
@@ -3,6 +3,7 @@ package com.qindel.practicas.practica1.controllers;
 import com.qindel.practicas.practica1.apirest.TipoDto;
 import com.qindel.practicas.practica1.services.ITipoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,19 +14,22 @@ public class TipoController {
 
     @Autowired
     private ITipoService tipoService;
-
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("")
     public List<TipoDto> getTipos(){
         return tipoService.getAllTipos();
     }
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/{id-tipo-jjoo}")
     public TipoDto getTipoById(@PathVariable("id-tipo-jjoo") Integer idtipojjoo){
         return tipoService.getTipoById(idtipojjoo);
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/tipo")
     public TipoDto addTipo(@RequestBody TipoDto nuevoTipo) {
         return tipoService.addTipo(nuevoTipo);
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/tipo/{id-tipo-jjoo}")
     public TipoDto updateTipo(@PathVariable("id-tipo-jjoo") Integer idtipo, @RequestBody TipoDto nuevoTipo) {
         TipoDto tipoEditar = tipoService.getTipoById(idtipo);
@@ -34,7 +38,7 @@ public class TipoController {
 
         return tipoService.updateTipo(nuevoTipo, idtipo);
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/tipo/{id-tipo-jjoo}")
     public void deleteTipo(@PathVariable("id-tipo-jjoo") Integer idtipo) {
         tipoService.deleteTipo(idtipo);
